c7ccf0a51d71325287a05019d6d3827e9e8a5072
RLauraS <R_LauraS@yahoo.com.ar>
2023-03-09 14:33:23 -0300

    Se corrige tipo de variable declarada

--- js/Ejercicio1.js
+++ js/Ejercicio1.js
@@ -1,5 +1,5 @@
 /*1- Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla en forma de lista  los doce nombres del arreglo. */
-let meses=['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+const meses=['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
 
 document.write('<ul>');
 for(let i=0; i< meses.length-1; i++){

850af0c3d2ac78c72424d20a40d10627331b43d9
RLauraS <R_LauraS@yahoo.com.ar>
2023-03-10 18:34:48 -0300

    Se inicia con el ejercicio 2

---
+++ js/Ejercicio2.js
@@ -0,0 +1,23 @@
+/*2-  Crear un script que solicite al usuario mediante un prompt el nombre de ciudades y almacenarlas en un arreglo, cuando el usuario selecciona cancelar se debe mostrar el arreglo generado, luego realizar las siguientes acciones:
+
+Mostrar la longitud del arreglo.
+Mostrar en el documento web los ítems de las posiciones primera, tercera y última.
+Añade en última posición la ciudad de París.
+Escribe por pantalla el elemento que ocupa la segunda posición.
+Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
+*/
+//crear arreglo
+let ciudades = [];
+//do-while hasta que se indique cancelar
+do {
+
+  //solicitar ingreso de ciudades
+  let ciudad = prompt("Ingrese una ciudad o presione cancelar para finalizar");
+  ciudades.push(ciudad);
+  //ciudades.lenght Mostrar la longitud del arreglo.
+  //Mostrar la longitud del arreglo.
+  //Añade en última posición la ciudad de París.
+  //Escribe por pantalla el elemento que ocupa la segunda posición.
+  //Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
+} while (confirm("¿Desea seguir agregando ciudades?"));
+document.write("Se ingresaron la/s ciudad/es: " + ciudades);
